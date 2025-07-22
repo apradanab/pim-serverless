d4fbc051c76f13d99322340cbc0f45fc2f81748b
apradanab <apradanab@gmail.com>
2025-07-22 21:01:52 +0200

    feat: Lambda createTherapy + DynamoDB + API Gateway

--- lib/pim-serverless-stack.ts
+++ lib/pim-serverless-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -14,6 +17,29 @@ export class PimServerlessStack extends cdk.Stack {
       sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-    })
+    });
+
+    const createTherapy = new lambda.Function(this, 'CreateTherapy', {
+      runtime: lambda.Runtime.NODEJS_LATEST,
+      handler: 'create-therapy.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda/handlers')),
+      environment: {
+        TABLE_NAME : this.dataTable.tableName,
+      },
+    });
+
+    this.dataTable.grantWriteData(createTherapy);
+
+    const api = new apigateway.RestApi(this, 'PimApi', {
+      restApiName: 'PIM Service',
+      description: 'Therapy management API',
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      },
+    });
+
+    const therapies = api.root.addResource('therapies');
+    therapies.addMethod('POST', new apigateway.LambdaIntegration(createTherapy));
   }
 }
