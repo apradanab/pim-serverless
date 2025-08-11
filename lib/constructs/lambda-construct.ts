e05ea003bec9eb04bedcb6b2dedc8905744390c3
apradanab <apradanab@gmail.com>
2025-08-11 22:09:25 +0200

    core: update constructs for renamed handlers

--- lib/constructs/lambda-construct.ts
+++ lib/constructs/lambda-construct.ts
@@ -12,17 +12,20 @@ interface LambdaConstructProps {
 
 type LambdaHandlers = {
   createTherapy: NodejsFunction;
-  getAllTherapies: NodejsFunction;
-  getTherapyById: NodejsFunction;
+  listTherapies: NodejsFunction;
+  getTherapy: NodejsFunction;
   updateTherapy: NodejsFunction;
   deleteTherapy: NodejsFunction;
+
   createAdvice: NodejsFunction;
-  getAllAdvices: NodejsFunction;
-  getAdviceById: NodejsFunction;
-  getAdvicesByTherapyId: NodejsFunction;
+  listAdvices: NodejsFunction;
+  getAdvice: NodejsFunction;
+  listAdvicesByTherapy: NodejsFunction;
   updateAdvice: NodejsFunction;
   deleteAdvice: NodejsFunction;
+
   createAppointment: NodejsFunction;
+  
   mediaUpload: NodejsFunction;
 };
 
@@ -44,31 +47,31 @@ export class LambdaConstruct extends Construct {
     }
 
     this.handlers = {
-      createTherapy: this.createHandler('CreateTherapy', 'therapies/create-therapy.ts', commonProps),
-      getAllTherapies: this.createHandler('GetAllTherapies', 'therapies/get-all-therapies.ts', commonProps),
-      getTherapyById: this.createHandler('GetTherapyById', 'therapies/get-therapy-by-id.ts', commonProps),
-      updateTherapy: this.createHandler('UpdateTherapy', 'therapies/update-therapy.ts', commonProps),
-      deleteTherapy: this.createHandler('DeleteTherapy', 'therapies/delete-therapy.ts', commonProps),
-      createAdvice: this.createHandler('CreateAdvice', 'advices/create-advice.ts', commonProps),
-      getAllAdvices: this.createHandler('GetAllAdvices', 'advices/get-all-advices.ts', commonProps),
-      getAdviceById: this.createHandler('GetAdviceById', 'advices/get-advice-by-id.ts', commonProps),
-      getAdvicesByTherapyId: this.createHandler('GetAdvicesByTherapyId', 'advices/get-advices-by-therapyId.ts', commonProps),
-      updateAdvice: this.createHandler('UpdateAdvice', 'advices/update-advice.ts', commonProps),
-      deleteAdvice: this.createHandler('DeleteAdvice', 'advices/delete-advice.ts', commonProps),
-      createAppointment: this.createHandler('CreateAppointment', 'appointments/create-appointment.ts', commonProps),
+      createTherapy: this.createHandler('CreateTherapy', 'therapies/create.ts', commonProps),
+      listTherapies: this.createHandler('ListTherapies', 'therapies/list.ts', commonProps),
+      getTherapy: this.createHandler('GetTherapy', 'therapies/get.ts', commonProps),
+      updateTherapy: this.createHandler('UpdateTherapy', 'therapies/update.ts', commonProps),
+      deleteTherapy: this.createHandler('DeleteTherapy', 'therapies/delete.ts', commonProps),
+      createAdvice: this.createHandler('CreateAdvice', 'advices/create.ts', commonProps),
+      listAdvices: this.createHandler('ListAdvices', 'advices/list.ts', commonProps),
+      getAdvice: this.createHandler('GetAdvice', 'advices/get.ts', commonProps),
+      listAdvicesByTherapy: this.createHandler('ListAdvicesByTherapy', 'advices/list-by-therapy.ts', commonProps),
+      updateAdvice: this.createHandler('UpdateAdvice', 'advices/update.ts', commonProps),
+      deleteAdvice: this.createHandler('DeleteAdvice', 'advices/delete.ts', commonProps),
+      createAppointment: this.createHandler('CreateAppointment', 'appointments/create.ts', commonProps),
       mediaUpload: this.createHandler('MediaUpload', 'core/media-upload.ts', commonProps)
     };
 
     const table = props.dbConstruct.dataTable;
     table.grantWriteData(this.handlers.createTherapy);
-    table.grantReadData(this.handlers.getAllTherapies);
-    table.grantReadData(this.handlers.getTherapyById);
+    table.grantReadData(this.handlers.listTherapies);
+    table.grantReadData(this.handlers.getTherapy);
     table.grantReadWriteData(this.handlers.updateTherapy);
     table.grantWriteData(this.handlers.deleteTherapy);
     table.grantWriteData(this.handlers.createAdvice);
-    table.grantReadData(this.handlers.getAllAdvices);
-    table.grantReadData(this.handlers.getAdviceById);
-    table.grantReadData(this.handlers.getAdvicesByTherapyId);
+    table.grantReadData(this.handlers.listAdvices);
+    table.grantReadData(this.handlers.getAdvice);
+    table.grantReadData(this.handlers.listAdvicesByTherapy);
     table.grantReadWriteData(this.handlers.updateAdvice);
     table.grantWriteData(this.handlers.deleteAdvice);
     table.grantWriteData(this.handlers.createAppointment);
@@ -83,7 +86,7 @@ export class LambdaConstruct extends Construct {
   ): NodejsFunction {
     return new NodejsFunction(this, id, {
       ...props,
-      entry: path.join(__dirname, '..', '..', 'lambda', 'handlers', handlerFile),
+      entry: path.join(__dirname, '..', '..', 'functions', handlerFile),
       handler: 'handler',
     });
   }
