7db6fe20929b64439a4b947d1265e100e444003f
apradanab <apradanab@gmail.com>
2025-09-21 23:12:24 +0200

    refactor(api): extract auth and media routes into subconstructs

---
+++ lib/constructs/api/media-routes.ts
@@ -0,0 +1,25 @@
+import { Construct } from 'constructs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+
+interface MediaRoutesProps {
+  api: apigateway.RestApi;
+  authorizer: apigateway.CognitoUserPoolsAuthorizer;
+  handlers: {
+    mediaUpload: NodejsFunction;
+  };
+}
+
+export class MediaRoutesConstruct extends Construct {
+  constructor(scope: Construct, id: string, props: MediaRoutesProps) {
+    super(scope, id);
+
+    const media = props.api.root.addResource('media');
+    const mediaType = media.addResource('{type}');
+    const mediaTypeAndId = mediaType.addResource('{id}');
+    mediaTypeAndId.addMethod('PUT', new apigateway.LambdaIntegration(props.handlers.mediaUpload), {
+      authorizer: props.authorizer,
+      authorizationType: apigateway.AuthorizationType.COGNITO,
+    });
+  }
+}
