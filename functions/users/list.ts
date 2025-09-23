2caad97ade068be7c61d87e75bcfcdeea9d805ab
apradanab <apradanab@gmail.com>
2025-09-23 23:51:52 +0200

    feat(handler): restrict listUsers to admin via Cognito claims

--- functions/users/list.ts
+++ functions/users/list.ts
@@ -4,7 +4,23 @@ import { User } from "../shared/types/user";
 
 const dbService = new DatabaseService<User>(process.env.TABLE_NAME!);
 
-export const handler = async (): Promise<ApiResponse> => {
+export const handler = async (event: {
+  requestContext?: {
+    authorizer?: {
+      claims?: {
+        email?: string;
+        sub?: string;
+        ['cognito:groups']?: string;
+      };
+    };
+  };
+}): Promise<ApiResponse> => {
+  const groups = event.requestContext?.authorizer?.claims?.["cognito:groups"] || '';
+
+  if (!groups.includes('ADMIN')) {
+    return error(403, 'Only Admins are authorized to list users')
+  }
+
   try {
     const users = await dbService.queryByType('User');
     return success(users);
